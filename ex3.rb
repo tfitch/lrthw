da8acbfe86850ff2a6d03a4a356879559663c69d
tfitch <c9@tylerfitch.com>
2014-12-30 18:36:49 +0000

    example 3.  Adding a .0 makes an integer a float

--- ex3.rb
+++ ex3.rb
@@ -1,23 +1,37 @@
+# text output
 puts "I will now count my chickens:"
 
-puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
+# 25 + (30 / 6)
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# 100 - (75 % 4) -> 100 - 3 = 97
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
+# text output
 puts "Now I will count the eggs:"
 
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# (3 + 2 + 1 - 5) + (4 % 2) - (1 / 4) + 6 -> 1 + 0 - 0 + 6 = 7
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
+# text output
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# 5 < -2 -> false
 puts 3 + 2 < 5 - 7
 
+# inline evaluation to 5
 puts "What is 3 + 2? #{3 + 2}"
+# inline evaluation to -2
 puts "What is 5 - 7? #{5 - 7}"
 
+# text output
 puts "Oh, that's why it's false."
 
+# text output
 puts "How about some more."
 
-puts "Is it great? #{5 > -2}"
-puts "Is it great or equal? #{5 >= -2}"
+# the opposite comparison from above
+puts "Is it greater? #{5 > -2}"
+# another comparison option
+puts "Is it greater or equal? #{5 >= -2}"
+# and another
 puts "Is it less or equal? #{5 <= -2}"
