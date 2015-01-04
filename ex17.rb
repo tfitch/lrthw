3f44176b9e22f913ecd682484df0e5a715f94e2e
tfitch <c9@tylerfitch.com>
2015-01-04 19:16:38 +0000

    exercise 17. write a file by reading a file

---
+++ ex17.rb
@@ -0,0 +1,16 @@
+from_file, to_file = ARGV
+
+puts "Copying from #{from_file} to #{to_file}"
+
+# we could do these two on one line, how?
+in_file = open(from_file); indata = in_file.read
+
+puts "The input file is #{indata.length} bytes long"
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+puts "Alright, all done."
+
+out_file.close
+in_file.close
