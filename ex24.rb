315d4309d9a0d3a80aab55e96c74abeeaf946eab
tfitch <c9@tylerfitch.com>
2015-01-12 00:17:06 +0000

    exercise 25. Defined a module.  (and a typo in ex24)

--- ex24.rb
+++ ex24.rb
@@ -4,7 +4,7 @@ puts 'You\'d need to know \'bout\' escapes with \\ to do \n newlines and \t tabs
 poem = <<END
 \tThe lovely world
 with logic so firmly planted
-cannot discern \n the needs of lovely
+cannot discern \n the needs of love
 nor comprehend passion from intuition
 and requires an explanation
 \n\t\twhere there is none.
